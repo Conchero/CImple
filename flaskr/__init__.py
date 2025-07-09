88de2647b05f876d908080f1356e51a871cc9f61
Conchero <florian.parra@hotmail.com>
2025-07-09 19:16:19 +0200

    replaced instance path

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -5,7 +5,7 @@ from flask import Flask
 def create_app(test_config = None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.instance_path = "./data/instance"
+    app.instance_path = "./data"
     database_path = os.path.join(app.instance_path, 'flaskr.sqlite')
     app.config.from_mapping(
         SECRET_KEY='dev',
