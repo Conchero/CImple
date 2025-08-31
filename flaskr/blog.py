4f4240ba3cac2153593585fa3daea10f7b971b70
Conchero <florian.parra@hotmail.com>
2025-08-31 20:41:17 +0200

    Category Added On Create

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -21,6 +21,9 @@ def index():
 @bp.route('/create', methods=('GET', 'POST'))
 @login_required
 def create():
+    
+    db = get_db()
+    categories = db.execute('SELECT * FROM category').fetchall()
     if request.method == 'POST':
         title = request.form['title']
         body = request.form['body']
@@ -41,7 +44,7 @@ def create():
             db.commit()
             return redirect(url_for('blog.index'))
 
-    return render_template('blog/create.html')
+    return render_template('blog/create.html', categories=categories)
 
 def get_post(id, check_author=True):
     post = get_db().execute(
