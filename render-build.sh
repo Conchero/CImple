17b09a88f227607f81037959f950aec2d4792f9d
Conchero <florian.parra@hotmail.com>
2025-09-10 14:37:42 +0200

    fix in render build .sh

--- render-build.sh
+++ render-build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-# créer le dossier /instance s’il n’existe pas
-mkdir -p instance
 
 # initialiser la base si elle n’existe pas
 if [ ! -f /instance/flaskr.sqlite ]; then
+# créer le dossier /instance s’il n’existe pas
+mkdir -p instance
   flask --app flaskr init-db
 fi
 
